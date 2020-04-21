3a32378f320d803b599da3e9a97fc4c17585fac0
Ashwin <ashwinsubramaniam66@users.noreply.github.com>
2020-04-20 19:52:11 -0500

    SingleNumber

---
+++ SingleNumber.java
@@ -0,0 +1,9 @@
+class SingleNumber {
+    public int singleNumber(int[] nums) {
+        int t=0;
+        for(int i=0;i<nums.length;i++){
+            t=t^nums[i];
+        }
+        return t;       
+    }
+}
